a9dceed091e63f4aa394b7e2c45416822578f04d
akshar <glaivesharp@gmail.com>
2023-10-01 00:36:28 +0530

    Specify media folder

--- project/settings.py
+++ project/settings.py
@@ -125,6 +125,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# path for serving media files
+MEDIA_URL = '/media/'
+
+# path for storing media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
